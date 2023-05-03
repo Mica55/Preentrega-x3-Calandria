0b52926bad9d0ba58467fad4a613fdcc126cd5e4
Mica55 <109596446+Mica55@users.noreply.github.com>
2023-05-03 19:11:24 -0300

    Pre entrega 3 Calandria

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Poyecto de Obra social:
 
 sus funcionalidades para el usuario es que pueda sacar turnos con los especialistas asignados
 los datos por ahora el usuario los ingresara a traves de promp por alertas que le salen en pantalla 
+A continuacion creo objetos con datos personales de pacientes, busco dentro de esos objetos datos especificos
+creo un array para consultarte al cliente a traves del promp que analisi quiere hacerse 
+agregue funcion de orden superior como mat, foreach 
--- script/main.js
+++ script/main.js
@@ -1,29 +1,76 @@
-let nombre= prompt("Ingrese su nombre")
-let apellido= prompt("Ingrese su apellido")
-let edad = parseInt(prompt("¿Que edad tienes?"));
+const datospersonales = document.getElementsByClassName("datos");
+function checkName(name) {
+  if (typeof name != 'string') return true
+  if (!isNaN(name)) return true
+  if (name.length < 2) return true
+  if (name.includes(' ')) return true
+  return false
 
-if (edad >= 18) {
-	alert("Bienvenido al sitio");
-  console.log("Puedes ingresar al sitio");
-  
-  //Si es mayor
-  let profesional = prompt("¿ Con que profesional desea atenderse?")
-  alert("Con" +  profesional + "solo daremos 5 turnos")
-  console.log("con" +  profesional  + "solo daremos 5 turnos");
-  //Si es menor
-} else {
-	alert("No puedes ingresar al sitio, no eres mayor de edad. Por favor vuelva con un mayor. Presione Aceptar y luego F5 para recargar la página");
-	console.log("No puedes ingresar al sitio, no eres mayor de edad.");
 }
-let mail = prompt("Ingrese su mail para recibir numero de turno")
-console.log("el usuario ingresa mail");
-let turno = 1;
-for (let turno = 1; turno <= 5; turno++);{
-    alert("Su numero es" +  turno + "Revise su mail, Muchas gracias")
-    console.log("Solo 5 turnos entregados")
+let datos = document.getElementsByClassName("datos");
+for (let dato of datos) {
+  console.log(dato.innerHTML);
+};
+
+const DatosdePacientes1 = {
+  nombre: "Micaela",
+  apellido: "Calandria",
+  DNI: 36333444,
+  tipodesangre: "B+",
+
 }
-function saludo(bienvenido) {
-  console.log("Hola"  +  bienvenido );
-  
- }
- saludo("bienvenido"  +  nombre );
+console.log("Observo lo que esta dentro de mi objeto" + " " + DatosdePacientes1.tipodesangre);
+
+const DatosdePacientes2 = {
+  nombre: "Juan",
+  apellido: "Gonzalez",
+  DNI: 44222555,
+  tipodesangre: "A+",
+}
+
+console.log("tipo de sangre de paciente Juan Gonzalez" + " " + DatosdePacientes2.tipodesangre);
+
+//clases
+class Paciente3 {
+  constructor(nombre, apellido, DNI, tipodesangre) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.DNI = DNI;
+    this.tipodesangre = tipodesangre;
+  }
+}
+const Paciente = new Paciente3("Pedro", "Perez", 44555666, "0-");
+console.log("DNI de" + Paciente3);
+
+//Array
+const ANALISIS = [
+  { nombre: "Sangre", id: 1 },
+  { nombre: "Orina", id: 2 },
+  { nombre: "Radiografia", id: 3 },
+  { nombre: "Ecografia", id: 4 },
+  { nombre: "Mamografia", id: 5 },
+];
+ANALISIS.forEach((estudios) => {
+  console.log(estudios.nombre);
+
+  const listadeestudios = document.getElementById("Estudios");
+  listadeestudios.innerHTML = "<option>Seleccione un estudio para realizarse</option>"
+  ANALISIS.forEach((opciones) => {
+    let item = document.createElement("option");
+    item.value = opciones.id.toString();
+    item.innerText = opciones.nombre;
+    listadeestudios.append(item);
+  });
+
+
+});
+const botonenviar = document.getElementById("Enviar");
+botonenviar.onclick = () => { alert("Estudio Recibido")};
+localStorage.setItem("analisis", JSON.stringify(ANALISIS));
+
+let infoenlocalstorage = JSON.parse(localStorage.getItem('analisis'))
+
+
+
+
+
